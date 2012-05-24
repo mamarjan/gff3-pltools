5dbe4521c564a83ca97860ce41314e110c31445b
Marjan Povolni <marian.povolny@gmail.com>
2012-05-24 09:31:07 +0200

    Removed example file contents from lines feature file

--- features/iterate-over-lines-feature.rb
+++ features/iterate-over-lines-feature.rb
@@ -1,11 +1,5 @@
 TEST_FILENAME="features/data/iterate-over-lines.gff3"
 
-TEST_LINES = <<-eos
-ENSRNOG00000019422  Ensembl gene  27333567  27357352  . + . ID=ENSRNOG00000019422;Dbxref=taxon:10116;organism=Rattus norvegicus;chromosome=18;name=EGR1_RAT;source=UniProtKB/Swiss-Prot
-TRAN00000017239 ASTD  transcript  27344088  27346461  . + . ID=TRAN00000017239;Parent=ENSRNOG00000019422
-EXON00000131935 ASTD  exon  27344088  27344141  . + . ID=EXON00000131935;Parent=TRAN00000017239
-eos
-
 Given /^I have a GFF3 file on the disk$/ do
   File.exists?(TEST_FILENAME).should be_true
 end
@@ -19,7 +13,7 @@ Then /^I should be able to use lines\.each to iterate over lines$/ do
   @test_file.lines.each do |line|
     tmp_list.push line
   end
-  TEST_LINES.lines.map { |line| line.chomp }.should == tmp_list
+  File.open(TEST_FILENAME).readlines.map { |line| line.chomp }.should == tmp_list
   @test_file.close
 end
 
