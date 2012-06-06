4f9f44bb2af07df864c768f07e3f69de5d752ce3
Marjan Povolni <marian.povolny@gmail.com>
2012-06-06 18:55:18 +0200

    Validation utility now working

--- dlib/bin/validate_gff3.d
+++ dlib/bin/validate_gff3.d
@@ -15,7 +15,7 @@ void main(string[] args) {
     print_usage();
   }
 
-  foreach(rec; bio.gff3_file.open(to!string(filename), NO_VALIDATION)) {}
+  foreach(rec; bio.gff3_file.open(to!string(filename), WARNINGS_ON_ERROR)) {}
 }
 
 void print_usage() {
