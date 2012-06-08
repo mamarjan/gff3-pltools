86d25d9113109c63f31253d263f5b507cb1ece7c
Marjan Povolni <marian.povolny@gmail.com>
2012-06-08 21:21:58 +0200

    Refactored benchmark tool and added -r and -v options.

--- dlib/bin/benchmark_gff3.d
+++ dlib/bin/benchmark_gff3.d
@@ -1,21 +1,35 @@
-import std.stdio, std.file, std.conv;
+import std.stdio, std.file, std.conv, std.getopt;
 import bio.gff3_file, bio.gff3_validation;
 
 void main(string[] args) {
-  // Check if one argument was passed
-  if (args.length != 2)
-    print_usage();
+  // Parse command line arguments
+  bool replace_escaped_chars = false;
+  bool validate = false;
+  getopt(args,
+      std.getopt.config.passThrough,
+      "r", &replace_escaped_chars,
+      "v", &validate);
 
-  alias char[] array;
-  auto filename = to!array(args[1]);
-
-  // Check if file exists
-  if (!(filename.exists)) {
-    writeln("Count not find file: " ~ filename ~ "\n");
+  // Only a filename should be left at this point
+  auto filename = args[1];
+  if (args.length != 2) {
     print_usage();
+    return; // Exit the application
   }
 
-  foreach(rec; bio.gff3_file.open(to!string(filename), NO_VALIDATION, false)) {}
+  // Check if file exists
+  alias char[] array;
+  if (!(to!array(filename).exists)) {
+    writeln("Could not find file: " ~ filename ~ "\n");
+    print_usage();
+    return;
+  }
+
+  // Open file and loop over all records
+  auto records = bio.gff3_file.open(filename,
+                                    validate ? WARNINGS_ON_ERROR : NO_VALIDATION,
+                                    replace_escaped_chars);
+  foreach(rec; records) {}
 }
 
 void print_usage() {
