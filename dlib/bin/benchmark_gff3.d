4b4684219f7749fc79e50edbc3abfc335db5174b
Marjan Povolni <marian.povolny@gmail.com>
2012-06-11 20:32:08 +0200

    Additional cleanup

--- dlib/bin/benchmark_gff3.d
+++ dlib/bin/benchmark_gff3.d
@@ -1,9 +1,11 @@
 import std.stdio, std.file, std.conv, std.string;
 
+File input_file;
+
 void main(string[] args) {
-  auto data = new SplitFile(File(args[1], "r"));
+  input_file = File(args[1], "r");
   while(true) {
-    data.next_item();
+    next_item();
     try_and_catch();
   }
 }
@@ -15,40 +17,26 @@ void try_and_catch() {
   }
 }
 
-class SplitFile {
-  /**
-   * The constructor receives the file struct as a parameter and
-   * a chunk_size parameter, which is the size of the block which
-   * is retrieved from the file at once.
-   */
-  this(File input_file) {
-    this.input_file = input_file;
-  }
+char[] current_chunk;
 
-  void next_item() {
-    char[] line;
-    bool line_complete = false;
-    while (!line_complete) {
-      if (current_chunk.length == 0) {
-        current_chunk = new char[65536];
-        current_chunk = input_file.rawRead(current_chunk);
+void next_item() {
+  char[] line;
+  bool line_complete = false;
+  while (!line_complete) {
+    if (current_chunk.length == 0) {
+      current_chunk = new char[8177];
+      current_chunk = input_file.rawRead(current_chunk);
+    } else {
+      auto newline_index = current_chunk.indexOf('\n');
+      if (newline_index != -1) {
+        line ~= current_chunk[0..newline_index];
+        current_chunk = current_chunk[newline_index+1..$];
+        line_complete = true;
       } else {
-        auto newline_index = current_chunk.indexOf('\n');
-        if (newline_index != -1) {
-          line ~= current_chunk[0..newline_index];
-          current_chunk = current_chunk[newline_index+1..$];
-          line_complete = true;
-        } else {
-          line = current_chunk;
-          current_chunk = null;
-        }
+        line = current_chunk;
+        current_chunk = null;
       }
     }
-  }
-
-  private {
-    File input_file;
-    char[] current_chunk;
   }
 }
 
