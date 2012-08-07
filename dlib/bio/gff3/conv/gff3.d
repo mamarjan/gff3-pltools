9037aac46156fa2b736626e0bdb75aab33f8b7af
Marjan Povolni <marian.povolny@gmail.com>
2012-08-07 11:40:22 +0200

    Fixed GFF3 output bug

--- dlib/bio/gff3/conv/gff3.d
+++ dlib/bio/gff3/conv/gff3.d
@@ -5,13 +5,7 @@ import bio.gff3.record_range;
 
 bool to_gff3(GenericRecordRange records, File output, long at_most = -1) {
   long counter = 0;
-  bool first_attr = true;
   foreach(rec; records) {
-    if (!first_attr)
-      output.write(',');
-    else
-      first_attr = false;
-
     output.writeln(rec.toString(DataFormat.GFF3));
     counter += 1;
 
