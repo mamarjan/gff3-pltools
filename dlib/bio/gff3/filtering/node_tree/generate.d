d210e4f7fe3862d432da8ae451ebeac5158c5abd
Marjan Povolni <marian.povolny@gmail.com>
2012-10-15 20:11:14 +0200

    Extracted node tree generation from module filtering

---
+++ dlib/bio/gff3/filtering/node_tree/generate.d
@@ -0,0 +1,160 @@
+module bio.gff3.filtering.node_tree.generate;
+
+import bio.gff3.filtering.node_tree.node;
+
+/*******************************************************************************
+ * The following part if about generating a tree structure from a list
+ * of tokens.
+ ******************************************************************************/
+
+Node generate_tree(string[] tokens) {
+  Node root;
+
+  if (tokens.length == 0) {
+    root = new Node(NodeType.NONE);
+  } else {
+    Node current_root;
+    while (tokens.length != 0)
+      current_root = parse_next_token(current_root, tokens);
+    root = current_root;
+  }
+
+  return root;
+}
+
+unittest {
+  assert(generate_tree([]) !is null);
+  assert(generate_tree(null) !is null);
+}
+
+Node parse_next_token(Node left, ref string[] tokens) {
+  Node node;
+
+  string token = tokens[0];
+  tokens = tokens[1..$];
+  switch(token) {
+    case "field":
+      node = new Node(NodeType.FIELD_OPERATOR);
+      node.text = token;
+      if (left !is null) {
+        throw new Exception(token ~ "operator doesn't accept a value on the left");
+      } else if (tokens.length == 0) {
+        throw new Exception(token ~ " operator needs a field name to the right of it");
+      } else {
+        node.parameter = tokens[0];
+        tokens = tokens[1..$];
+      }
+      break;
+    case "attr", "attrib", "attribute":
+      node = new Node(NodeType.ATTR_OPERATOR);
+      node.text = token;
+      if (left !is null) {
+        throw new Exception(token ~ "operator doesn't accept a value on the left");
+      } else if (tokens.length == 0) {
+        throw new Exception(token ~ " operator needs an attribute name to the right of it");
+      } else {
+        node.parameter = tokens[0];
+        tokens = tokens[1..$];
+      }
+      break;
+    case "not":
+      node = new Node(NodeType.NOT_OPERATOR);
+      node.text = token;
+      if (left !is null) {
+        throw new Exception(token ~ "operator doesn't accept a value on the left");
+      } else if (tokens.length == 0) {
+        throw new Exception(token ~ " operator requires a boolean value to to the right of it");
+      } else {
+        auto right = parse_next_token(null, tokens);
+        node.children = [right];
+        right.parent = node;
+      }
+      break;
+    case "==", "!=", ">=", "<=", ">", "<", "and", "or", "+", "-", "*", "/", "contains", "starts_with":
+      switch(token) {
+        case "=="  : node = new Node(NodeType.EQUALS_OPERATOR); break;
+        case "!="  : node = new Node(NodeType.NOT_EQUALS_OPERATOR); break;
+        case "<="  : node = new Node(NodeType.LOWER_THAN_OR_EQUALS_OPERATOR); break;
+        case ">="  : node = new Node(NodeType.GREATER_THAN_OR_EQUALS_OPERATOR); break;
+        case ">"   : node = new Node(NodeType.GREATER_THAN_OPERATOR); break;
+        case "<"   : node = new Node(NodeType.LOWER_THAN_OPERATOR); break;
+        case "and" : node = new Node(NodeType.AND_OPERATOR); break;
+        case "or"  : node = new Node(NodeType.OR_OPERATOR); break;
+        case "+"   : node = new Node(NodeType.PLUS_OPERATOR); break;
+        case "-"   : node = new Node(NodeType.MINUS_OPERATOR); break;
+        case "*"   : node = new Node(NodeType.MULTIPLICATION_OPERATOR); break;
+        case "/"   : node = new Node(NodeType.DIVISION_OPERATOR); break;
+        case "contains"    : node = new Node(NodeType.CONTAINS_OPERATOR); break;
+        case "starts_with" : node = new Node(NodeType.STARTS_WITH_OPERATOR); break;
+        default:
+          throw new Exception("Error in the code, please report to the maintainer");
+          break;
+      }
+      node.text = token;
+      if (left is null) {
+        throw new Exception(token ~ " operator requires a value on the left");
+      } else if (tokens.length == 0) {
+        throw new Exception(token ~ " operator requires a value on the right");
+      } else {
+        auto right = parse_next_token(null, tokens);
+        node.children = [left, right];
+        left.parent = node;
+        right.parent = node;
+      }
+      break;
+    case "(":
+      node = new Node(NodeType.BRACKETS);
+      node.text = token;
+      if (left !is null) {
+        throw new Exception("brackets can't have a value on the left");
+      } else if (tokens.length == 0) {
+        throw new Exception("no closing bracket");
+      } else if (tokens[0] == ")") {
+        throw new Exception("brackets can't work without a value being devised inside them");
+      } else {
+        Node right;
+        while ((tokens.length != 0) && (tokens[0] != ")")) {
+          right = parse_next_token(right, tokens);
+        }
+        if (tokens.length == 0)
+          throw new Exception("no closing bracket");
+        tokens = tokens[1..$];
+        node.children = [right];
+        right.parent = node;
+      }
+      break;
+    case ")":
+      throw new Exception("Unexpected )");
+      break;
+    default:
+      if (left !is null)
+        throw new Exception("Unknown operator: " ~ token);
+      node = new Node(NodeType.VALUE);
+      node.text = token;
+      break;
+  }
+
+  return node;
+}
+
+unittest {
+  string[] tokens;
+  tokens = ["field", "test"];
+  Node node = parse_next_token(null, tokens);
+  assert(node !is null);
+  assert(node.type == NodeType.FIELD_OPERATOR);
+  assert(node.parameter == "test");
+  assert(node.parent is null);
+  assert(node.children.length == 0);
+
+  tokens = ["attr", "test"];
+  node = parse_next_token(null, tokens);
+  assert(node !is null);
+  assert(node.type == NodeType.ATTR_OPERATOR);
+  assert(node.parameter == "test");
+  assert(node.parent is null);
+  assert(node.children.length == 0);
+}
+
+
+
---
+++ dlib/bio/gff3/filtering/node_tree/node.d
@@ -0,0 +1,40 @@
+module bio.gff3.filtering.node_tree.node;
+
+package:
+
+enum NodeType {
+  NONE,
+  VALUE,
+  FIELD_OPERATOR,
+  ATTR_OPERATOR,
+  AND_OPERATOR,
+  OR_OPERATOR,
+  NOT_OPERATOR,
+  CONTAINS_OPERATOR,
+  STARTS_WITH_OPERATOR,
+  EQUALS_OPERATOR,
+  NOT_EQUALS_OPERATOR,
+  GREATER_THAN_OPERATOR,
+  LOWER_THAN_OPERATOR,
+  GREATER_THAN_OR_EQUALS_OPERATOR,
+  LOWER_THAN_OR_EQUALS_OPERATOR,
+  BRACKETS,
+  PLUS_OPERATOR,
+  MINUS_OPERATOR,
+  MULTIPLICATION_OPERATOR,
+  DIVISION_OPERATOR
+}
+
+class Node {
+  this(NodeType type) {
+    this.type = type;
+  }
+
+  NodeType type;
+  string text;
+  string parameter;
+  Node parent;
+  Node[] children;
+}
+
+
